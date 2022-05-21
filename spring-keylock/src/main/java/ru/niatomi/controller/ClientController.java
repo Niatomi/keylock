386cd000915c7687dc858bc61248ae231236bdca
Niatomi <playervoker@gmail.com>
2022-05-22 00:37:25 +0400

    Annotations moved to interface and add some methods

--- spring-keylock/src/main/java/ru/niatomi/controller/ClientController.java
+++ spring-keylock/src/main/java/ru/niatomi/controller/ClientController.java
@@ -1,7 +1,29 @@
 package ru.niatomi.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.niatomi.entity.OpenerEntity;
+
+import java.awt.print.Pageable;
+
 /**
  * @author niatomi
  */
+@RestController
+@RequestMapping("/client")
 public interface ClientController {
+
+    @PostMapping("/create")
+    ResponseEntity<String> create(@RequestBody OpenerEntity openerEntity);
+
+    @DeleteMapping("/delete/{id}")
+    ResponseEntity<String> deleteAccessor(@PathVariable Long id);
+
+    @PutMapping("/{id}")
+    ResponseEntity<String> update(@RequestBody OpenerEntity openerEntity, @PathVariable Long id);
+
+    @GetMapping("/openersWithAccess/{page}")
+    ResponseEntity<Page<OpenerEntity>> getOpenersWithAccess(@PathVariable Integer page,
+                                                            @RequestParam(defaultValue = "10") Integer size);
 }
--- spring-keylock/src/main/java/ru/niatomi/controller/Impl/ClientControllerImpl.java
+++ spring-keylock/src/main/java/ru/niatomi/controller/Impl/ClientControllerImpl.java
@@ -1,11 +1,14 @@
 package ru.niatomi.controller.Impl;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.niatomi.controller.ClientController;
 import ru.niatomi.entity.OpenerEntity;
 import ru.niatomi.exceptions.OpenerAlreadyExistsException;
 import ru.niatomi.service.ClientService;
@@ -13,21 +16,33 @@ import ru.niatomi.service.ClientService;
 /**
  * @author niatomi
  */
-@RestController
-@RequestMapping("/client")
-public class ClientControllerImpl {
+@AllArgsConstructor
+public class ClientControllerImpl implements ClientController {
 
-    @Autowired
-    private ClientService clientService;
+    private final ClientService clientService;
 
-    @PostMapping("/create")
-    public ResponseEntity create(@RequestBody OpenerEntity openerEntity) {
+    public ResponseEntity<String> create(OpenerEntity openerEntity) {
         try {
             clientService.create(openerEntity);
             return ResponseEntity.ok("User saved");
         } catch (OpenerAlreadyExistsException e) {
             return ResponseEntity.badRequest().body("Error happened");
         }
+    }
+
+    @Override
+    public ResponseEntity<String> deleteAccessor(Long id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<String> update(OpenerEntity openerEntity, Long id) {
+        return null;
+    }
+
+    @Override
+    public ResponseEntity<Page<OpenerEntity>> getOpenersWithAccess(Integer page, Integer size) {
+        return null;
     }
 
 }
