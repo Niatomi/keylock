ab0219603808fee0d2e6af80ebca02b74b2c275f
Niatomi <playervoker@gmail.com>
2022-05-22 00:55:06 +0400

    Add springdoc documentation

--- spring-keylock/src/main/java/ru/niatomi/controller/ClientController.java
+++ spring-keylock/src/main/java/ru/niatomi/controller/ClientController.java
@@ -1,6 +1,11 @@
 package ru.niatomi.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.data.domain.Page;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.niatomi.entity.OpenerEntity;
@@ -15,15 +20,40 @@ import java.awt.print.Pageable;
 public interface ClientController {
 
     @PostMapping("/create")
+    @Operation(description = "Request for creating new opener.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Request is ok.",
+                    content = {@Content(mediaType = MediaType.TEXT_PLAIN_VALUE)}),
+            @ApiResponse(responseCode = "400", description = "User already exists"),
+            @ApiResponse(responseCode = "406", description = "Validation failed")
+    })
     ResponseEntity<String> create(@RequestBody OpenerEntity openerEntity);
 
     @DeleteMapping("/delete/{id}")
+    @Operation(description = "Request for delete opener by id.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Request is ok.",
+                    content = {@Content(mediaType = MediaType.TEXT_PLAIN_VALUE)}),
+            @ApiResponse(responseCode = "400", description = "Client not found")
+    })
     ResponseEntity<String> deleteAccessor(@PathVariable Long id);
 
     @PutMapping("/{id}")
+    @Operation(description = "Request for update opener by id .")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Request is ok.",
+                    content = {@Content(mediaType = MediaType.TEXT_PLAIN_VALUE)}),
+            @ApiResponse(responseCode = "400", description = "User already exists"),
+            @ApiResponse(responseCode = "406", description = "Validation failed")
+    })
     ResponseEntity<String> update(@RequestBody OpenerEntity openerEntity, @PathVariable Long id);
 
     @GetMapping("/openersWithAccess/{page}")
+    @Operation(description = "Request for get in pages all of the openers.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Request is ok.",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE)}),
+    })
     ResponseEntity<Page<OpenerEntity>> getOpenersWithAccess(@PathVariable Integer page,
                                                             @RequestParam(defaultValue = "10") Integer size);
 }
--- spring-keylock/src/main/java/ru/niatomi/controller/ESPController.java
+++ spring-keylock/src/main/java/ru/niatomi/controller/ESPController.java
@@ -1,5 +1,10 @@
 package ru.niatomi.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.niatomi.model.ActionsHistory;
@@ -12,8 +17,19 @@ import ru.niatomi.model.ActionsHistory;
 public interface ESPController {
 
     @GetMapping
+
+    @Operation(description = "Request from ESP for get all passwords with opener containing the password.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Request is ok.",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE)}),
+    })
     ResponseEntity getPasswords();
 
     @PostMapping
+    @Operation(description = "Save action with keylock in DB.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Request is ok.",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE)}),
+    })
     void addActions(@RequestBody ActionsHistory actionsHistory);
 }
