8b6166070e24c51c967568f384cfe35515c268a6
Niatomi <playervoker@gmail.com>
2022-05-24 02:20:48 +0400

    Add exception checks to GlobalExceptionHandler

--- spring-keylock/src/main/java/ru/niatomi/exceptions/excep/GlobalExceptionHandler.java
+++ spring-keylock/src/main/java/ru/niatomi/exceptions/excep/GlobalExceptionHandler.java
@@ -1,7 +1,32 @@
 package ru.niatomi.exceptions.excep;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.niatomi.exceptions.OpenerAlreadyExistsException;
+
+import javax.validation.ConstraintViolationException;
+
 /**
  * @author niatomi
  */
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(OpenerAlreadyExistsException.class)
+    public ResponseEntity<ExceptionResponse> handleValidExceptions(OpenerAlreadyExistsException ex) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse();
+        exceptionResponse.setException(ex.getMessage());
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+
+
+    @ExceptionHandler({ConstraintViolationException.class})
+    public ResponseEntity<ExceptionResponse> handleValidationException(ConstraintViolationException ex) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse();
+        exceptionResponse.setException(ex.getMessage());
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_ACCEPTABLE);
+    }
+
 }
