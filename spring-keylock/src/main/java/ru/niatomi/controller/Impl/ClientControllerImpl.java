da2e536b63d99155e77bec03609b67ec0b9522ae
Niatomi <playervoker@gmail.com>
2022-05-27 04:59:50 +0400

    Deleted try/catch and annotations for controller

--- spring-keylock/src/main/java/ru/niatomi/controller/Impl/ClientControllerImpl.java
+++ spring-keylock/src/main/java/ru/niatomi/controller/Impl/ClientControllerImpl.java
@@ -1,49 +1,31 @@
 package ru.niatomi.controller.Impl;
 
 import lombok.AllArgsConstructor;
-import org.springframework.data.domain.Page;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.niatomi.controller.ClientController;
-import ru.niatomi.model.domain.OpenerEntity;
-import ru.niatomi.service.Impl.ClientServiceImpl;
-
-import java.util.List;
+import ru.niatomi.entity.KeylockConfig;
+import ru.niatomi.entity.OpenerEntity;
+import ru.niatomi.exceptions.OpenerAlreadyExistsException;
+import ru.niatomi.service.ClientService;
 
 /**
  * @author niatomi
  */
 @RestController
+@RequestMapping("/client")
 @AllArgsConstructor
 public class ClientControllerImpl implements ClientController {
 
-    private final ClientServiceImpl service;
+    private final ClientService clientService;
 
-    public ResponseEntity<String> create(OpenerEntity openerEntity) {
-        service.create(openerEntity);
+    @Override
+    public ResponseEntity create(OpenerEntity openerEntity) {
+        clientService.create(openerEntity);
         return ResponseEntity.ok("User saved");
     }
 
-    @Override
-    public ResponseEntity<String> deleteAccessor(Long id) {
-        return ResponseEntity.ok(service.delete(id));
-    }
 
-    @Override
-    public ResponseEntity<String> update(OpenerEntity openerEntity) {
-//        return ResponseEntity.update;
-        return null;
-    }
-
-
-    @Override
-    public ResponseEntity<Page<OpenerEntity>> getOpenersWithAccess(Integer page, Integer size) {
-        return null;
-    }
-
-    @Override
-    public ResponseEntity<List<OpenerEntity>> getOpeners() {
-        return null;
-    }
 
 }
