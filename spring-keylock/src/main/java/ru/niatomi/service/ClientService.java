e12a831d45da578461a6ccf4c4920278aedea3a9
Niatomi <playervoker@gmail.com>
2022-05-27 04:59:15 +0400

    Deleted throws exception in signature

--- spring-keylock/src/main/java/ru/niatomi/service/ClientService.java
+++ spring-keylock/src/main/java/ru/niatomi/service/ClientService.java
@@ -1,13 +1,33 @@
 package ru.niatomi.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import ru.niatomi.model.domain.OpenerEntity;
+import ru.niatomi.entity.OpenerEntity;
+import ru.niatomi.exceptions.OpenerAlreadyExistsException;
+import ru.niatomi.repository.OpenerRepository;
+import ru.niatomi.repository.PasswordRepository;
 
 /**
  * @author niatomi
  */
 @Service
-public interface ClientService {
-    OpenerEntity create(OpenerEntity openerEntity);
-    String delete(Long id);
+public class ClientService {
+
+    @Autowired
+    private OpenerRepository openerRepository;
+
+    @Autowired
+    private PasswordRepository passwordRepository;
+
+    public OpenerEntity create(OpenerEntity openerEntity) {
+        return openerRepository.save(openerEntity);
+    }
 }
+
+
+//    public UserEntity registration(UserEntity user) throws UserAlreadyExistsException {
+//        if (userRepo.findByUsername(user.getUsername()) != null) {
+//            throw new UserAlreadyExistsException(user);
+//        }
+//        return userRepo.save(user);
+//    }
