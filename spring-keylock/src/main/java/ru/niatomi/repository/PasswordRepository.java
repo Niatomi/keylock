66c0d3e14e1851921ec5246902dc3e4adde7f7f8
Niatomi <playervoker@gmail.com>
2022-06-02 00:38:04 +0400

    Add methods for deleting relationshipped from opener entity id

--- spring-keylock/src/main/java/ru/niatomi/repository/PasswordRepository.java
+++ spring-keylock/src/main/java/ru/niatomi/repository/PasswordRepository.java
@@ -3,9 +3,15 @@ package ru.niatomi.repository;
 import org.springframework.data.repository.CrudRepository;
 import ru.niatomi.model.domain.PasswordEntity;
 
+import java.util.Optional;
+
 /**
  * @author niatomi
  */
 public interface PasswordRepository extends CrudRepository<PasswordEntity, Long> {
-    PasswordEntity findByValueContaining(String password);
+
+    Optional<PasswordEntity> findByValueEquals(String value);
+
+    void deleteByOpenerIdEquals(Long id);
+
 }
