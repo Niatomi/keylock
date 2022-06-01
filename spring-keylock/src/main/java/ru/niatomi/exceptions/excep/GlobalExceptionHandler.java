6f0f22818ce6363f42680fc34ba9b2d900338593
Niatomi <playervoker@gmail.com>
2022-06-02 00:34:31 +0400

    Add exception handler methods

--- spring-keylock/src/main/java/ru/niatomi/exceptions/excep/GlobalExceptionHandler.java
+++ spring-keylock/src/main/java/ru/niatomi/exceptions/excep/GlobalExceptionHandler.java
@@ -3,10 +3,13 @@ package ru.niatomi.exceptions.excep;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import ru.niatomi.exceptions.OpenerAlreadyExistsException;
 
 import javax.validation.ConstraintViolationException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 /**
  * @author niatomi
@@ -15,18 +18,54 @@ import javax.validation.ConstraintViolationException;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(OpenerAlreadyExistsException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ExceptionResponse> handleValidExceptions(OpenerAlreadyExistsException ex) {
         ExceptionResponse exceptionResponse = new ExceptionResponse();
-        exceptionResponse.setException(ex.getMessage());
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+        exceptionResponse.setStatus(HttpStatus.BAD_REQUEST);
+        exceptionResponse.setDateStamp(LocalDateTime.now());
+        exceptionResponse.setMessage(ex.getMessage());
+        return ResponseEntity.badRequest().body(exceptionResponse);
+    }
+
+    @ExceptionHandler(OpenerNotFoundException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ExceptionResponse> handleValidExceptions(OpenerNotFoundException ex) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse();
+        exceptionResponse.setStatus(HttpStatus.BAD_REQUEST);
+        exceptionResponse.setDateStamp(LocalDateTime.now());
+        exceptionResponse.setMessage(ex.getMessage());
+        return ResponseEntity.badRequest().body(exceptionResponse);
+    }
+
+    @ExceptionHandler(PasswordIsNotUniqueException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ExceptionResponse> handleValidExceptions(PasswordIsNotUniqueException ex) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse();
+        exceptionResponse.setStatus(HttpStatus.BAD_REQUEST);
+        exceptionResponse.setDateStamp(LocalDateTime.now());
+        exceptionResponse.setMessage(ex.getMessage());
+        return ResponseEntity.badRequest().body(exceptionResponse);
+    }
+
+    @ExceptionHandler(InvalidFirstUserException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ExceptionResponse> handleValidExceptions(InvalidFirstUserException ex) {
+        ExceptionResponse exceptionResponse = new ExceptionResponse();
+        exceptionResponse.setStatus(HttpStatus.BAD_REQUEST);
+        exceptionResponse.setDateStamp(LocalDateTime.now());
+        exceptionResponse.setMessage(ex.getMessage());
+        return ResponseEntity.badRequest().body(exceptionResponse);
     }
 
 
     @ExceptionHandler({ConstraintViolationException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<ExceptionResponse> handleValidationException(ConstraintViolationException ex) {
         ExceptionResponse exceptionResponse = new ExceptionResponse();
-        exceptionResponse.setException(ex.getMessage());
-        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_ACCEPTABLE);
+        exceptionResponse.setStatus(HttpStatus.BAD_REQUEST);
+        exceptionResponse.setDateStamp(LocalDateTime.now());
+        exceptionResponse.setMessage(ex.getMessage());
+        return ResponseEntity.badRequest().body(exceptionResponse);
     }
 
 }
