64f33f78da16f7efd4b5223d30059323010ff4a5
Niatomi <playervoker@gmail.com>
2022-06-02 00:36:12 +0400

    Add find methods for pagination

--- spring-keylock/src/main/java/ru/niatomi/repository/OpenerRepository.java
+++ spring-keylock/src/main/java/ru/niatomi/repository/OpenerRepository.java
@@ -1,10 +1,20 @@
 package ru.niatomi.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import ru.niatomi.model.domain.OpenerEntity;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author niatomi
  */
 public interface OpenerRepository extends CrudRepository<OpenerEntity, Long> {
+
+    Page<OpenerEntity> findByPasswordsIsNotNull(Pageable page);
+
+    Page<OpenerEntity> findAll(Pageable page);
+
 }
