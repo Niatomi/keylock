bde7d1dc0fdd8da19a03a8ef612134f5f0753060
Niatomi <playervoker@gmail.com>
2022-06-02 00:47:26 +0400

    Add documentation into openApi

--- spring-keylock/src/main/java/ru/niatomi/controller/ESPController.java
+++ spring-keylock/src/main/java/ru/niatomi/controller/ESPController.java
@@ -1,5 +1,10 @@
 package ru.niatomi.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.niatomi.dto.KeylockConfigDto;
@@ -16,14 +21,26 @@ import java.util.List;
 public interface ESPController {
 
     @GetMapping
+    @Operation(description = "Request for getting all available passwords.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Request is ok.",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE)}),
+    })
     public ResponseEntity<List<PasswordWithOpenerIdDto>> getPasswords();
 
     @PostMapping
+    @Operation(description = "Request for writing new action into history.")
     void addActions(@RequestBody ActionsHistoryDto actionsHistory);
 
     @GetMapping("/getConfig")
+    @Operation(description = "Request for getting configuration of keylock.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Request is ok.",
+                    content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE)}),
+    })
     ResponseEntity<KeylockConfigDto> getConfig();
 
     @PostMapping("/setLock")
+    @Operation(description = "Request from keylock to keep blocking condition in data base.")
     void setLock();
 }
