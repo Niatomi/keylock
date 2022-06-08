7c6b45cccbb42076de483127451db93ef4430903
Niatomi <playervoker@gmail.com>
2022-06-08 23:44:46 +0400

    Add pageable method to return

--- spring-keylock/src/main/java/ru/niatomi/repository/ActionsHistoryRepository.java
+++ spring-keylock/src/main/java/ru/niatomi/repository/ActionsHistoryRepository.java
@@ -1,5 +1,7 @@
 package ru.niatomi.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import ru.niatomi.model.domain.ActionsHistoryEntity;
 
@@ -7,4 +9,5 @@ import ru.niatomi.model.domain.ActionsHistoryEntity;
  * @author niatomi
  */
 public interface ActionsHistoryRepository extends CrudRepository<ActionsHistoryEntity, Long> {
+    Page<ActionsHistoryEntity> findAll(Pageable page);
 }
