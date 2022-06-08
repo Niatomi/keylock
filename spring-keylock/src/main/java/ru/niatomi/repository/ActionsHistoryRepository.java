072d67efba75cb6a44b618763c3579bd9b02ee1c
Niatomi <playervoker@gmail.com>
2022-06-08 23:45:21 +0400

    Changed repo to PagingAndSortingRepository

--- spring-keylock/src/main/java/ru/niatomi/repository/ActionsHistoryRepository.java
+++ spring-keylock/src/main/java/ru/niatomi/repository/ActionsHistoryRepository.java
@@ -3,11 +3,12 @@ package ru.niatomi.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import ru.niatomi.model.domain.ActionsHistoryEntity;
 
 /**
  * @author niatomi
  */
-public interface ActionsHistoryRepository extends CrudRepository<ActionsHistoryEntity, Long> {
+public interface ActionsHistoryRepository extends PagingAndSortingRepository<ActionsHistoryEntity, Long> {
     Page<ActionsHistoryEntity> findAll(Pageable page);
 }
